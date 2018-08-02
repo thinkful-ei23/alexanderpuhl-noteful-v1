9759766700bc0efeeb5ab64d5b4e09a2b4441f6f
Alex Puhl <apuhlswimmer@hotmail.com>
2018-08-02 10:29:42 -0600

    updated delete to use promises

--- router/notes.router.js
+++ router/notes.router.js
@@ -71,31 +71,33 @@ router.post('/', (req, res, next) => {
     err.status = 400;
     return next(err);
   }
-  notes.create(newItem, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-    } else {
-      next();
-    }
-  });
+  notes.create(newItem)
+    .then(item => {
+      if (item) {
+        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 router.delete('/:id', (req, res, next) => {
   const id = req.params.id;
 
-  notes.delete(id, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (id) {
-      res.location(`http://${req.headers.host}/notes/${item.id}`).status(204).json('No Content');
-    } else {
-      next();
-    }
-  });
+  notes.delete(id)
+    .then(item => {
+      if (item) {
+        res.location(`http://${req.headers.host}/notes/${item.id}`).status(204).json('No Content');
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 
