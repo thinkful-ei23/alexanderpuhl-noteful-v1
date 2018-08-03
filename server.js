237a4371a4becbee442acd39d2f0d82cffc87d10
Alex Puhl <apuhlswimmer@hotmail.com>
2018-08-03 13:05:24 -0600

    setup Express static & 404 handler tests

--- server.js
+++ server.js
@@ -29,8 +29,12 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(PORT, function() {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.log(err);
-});
+if(require.main === module) {
+  app.listen(PORT, function() {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.log(err);
+  });
+}
+
+module.exports = app; // Export for testing
